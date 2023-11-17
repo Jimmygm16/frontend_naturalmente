e01f0dc261cdf63044c3fa2de7b7678ad6fa894e
jimmygm16 <jimmya.giraldom@autonoma.edu.co>
2023-11-17 18:48:02 -0500

    Jimmy #3

--- src/app/productos/[id]/page.tsx
+++ src/app/productos/[id]/page.tsx
@@ -2,14 +2,17 @@
 
 import { useState, useEffect } from "react";
 import { Product } from "@/types";
-import { getSingleProduct } from "@/services/productos";
+import { getSingleProduct } from "@/services/products";
 import { showCurrency } from "@/helpers";
 import { addProductToCart } from "@/services/users";
 import IncrementalButton from "@/app/components/IncrementalButton";
 
-export default function SingleProductPage({ params }: { params: { id: string } }): JSX.Element {
-
-  const [product, setProduct] = useState<Product>()
+export default function SingleProductPage({
+  params,
+}: {
+  params: { id: string };
+}): JSX.Element {
+  const [product, setProduct] = useState<Product>();
   const [price, setPrice] = useState<number>(0);
   const [quantity, setQuantity] = useState<number>(1);
 
@@ -24,69 +27,60 @@ export default function SingleProductPage({ params }: { params: { id: string } }
       }
     }
     fetchProduct();
-  }, [params.id])
+  }, [params.id]);
 
   const handlePrice = (productQuantity: number) => {
     setPrice(productQuantity * product!.price);
     setQuantity(productQuantity);
-  }
+  };
 
   const handleAddToCart = () => {
     async function addToCart() {
       try {
-        quantity > 1 
-          ? await addProductToCart(product!.id as number, { quantity: quantity })
-          : await addProductToCart(product!.id as number)
+        quantity > 1
+          ? await addProductToCart(product!.id as number, {
+              quantity: quantity,
+            })
+          : await addProductToCart(product!.id as number);
       } catch (error) {
         throw new Error("Error al agregar al carrito");
       }
     }
     addToCart();
-  }
+  };
 
   return (
     <>
       <section className="h-screen grid grid-cols-2 py-3">
-
-        <section className="w-full">
-
-        </section>
+        <section className="w-full"></section>
 
         <section className="flex flex-col w-[90%] mx-auto">
-
           <div className="font-semibold text-4xl py-6">
             <h1> {product?.product_name} </h1>
           </div>
 
           <div className="flex flex-row justify-between py-5 border-t-2 border-b-2 border-gray-200 items-center">
             <span className="font-semibold text-3xl text-color4">
-              { showCurrency(price) + " COP" } 
-              {/* {price > (product?.price as number) 
-                ? "  ( " + price / (product?.price as number) + " Unidades" + " )" 
+              {showCurrency(price) + " COP"}
+              {/* {price > (product?.price as number)
+                ? "  ( " + price / (product?.price as number) + " Unidades" + " )"
                 : null }  */}
             </span>
-            <IncrementalButton onUpdateQuantity={handlePrice}/>
+            <IncrementalButton onUpdateQuantity={handlePrice} />
           </div>
 
           <div className="py-5 text-xl">
-            <span>
-              {product?.product_description}
-            </span>
+            <span>{product?.product_description}</span>
           </div>
 
           <div className="flex flex-row gap-3 py-5">
-            <button className="btn w-full"
-              onClick={handleAddToCart}>
+            <button className="btn w-full" onClick={handleAddToCart}>
               Agregar al carrito
             </button>
-            <button className="btn w-full bg-orange-300">
-              Comprar
-            </button>
+            <button className="btn w-full bg-orange-300">Comprar</button>
           </div>
-
         </section>
-
       </section>
     </>
-  )
+  );
 }
--- src/app/productos/page.tsx
+++ src/app/productos/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { getProducts } from "@/services/productos";
+import { getProducts } from "@/services/products";
 import { Product } from "@/types";
 import ProductCard from "./ProductCard";
 
 export default function ProductsPage(): JSX.Element {
-
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
@@ -23,12 +22,13 @@ export default function ProductsPage(): JSX.Element {
 
   return (
     <div className="h-screen">
-
       <div className="mx-[7.5%] grid grid-cols-5 my-3">
         <form className="col-span-4">
-          <input className="w-full h-12 rounded-lg border-2 border-gray-300 focus:outline-none focus:border-green-700 px-3" 
-          type="text" 
-          placeholder="Buscar productos" />
+          <input
+            className="w-full h-12 rounded-lg border-2 border-gray-300 focus:outline-none focus:border-green-700 px-3"
+            type="text"
+            placeholder="Buscar productos"
+          />
         </form>
         <div className="col-span-1 ml-2">
           <button className="w-full h-12 bg-color4 text-white rounded-lg">
@@ -37,12 +37,11 @@ export default function ProductsPage(): JSX.Element {
         </div>
       </div>
 
-      <div className='flex flex-col divide-y-2 gap-3 divide-solid mx-[7.5%]'>
+      <div className="flex flex-col divide-y-2 gap-3 divide-solid mx-[7.5%]">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
-
     </div>
-  )
+  );
 }
