f6dc89bbbf7b7a33cfff968b3529107b48b26afe
SergioAristides <sergioa.riverab@autonoma.edu.co>
2023-11-30 13:09:45 -0500

    iniciando profile

--- src/API.ts
+++ src/API.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost/api_rest_naturalmente/public/api/v1',
+  baseURL: 'http://localhost:8000/api/v1',
 });
 
 export default API;
---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound()
+{
+    return <section className="h-screen">
+    <h1>404 - Página no encontrada</h1>
+    <p>¡Pero no te preocupes! Aquí tienes un juego para pasar el tiempo:</p>
+    <iframe src="https://play.unity.com/p/webgl-builds-379450/edit" width="800" height="600"></iframe>
+    </section>
+}
